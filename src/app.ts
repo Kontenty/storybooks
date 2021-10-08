92cb210edb3093f515f5959f12aa4e6ebd618bc1
Konrad Gorski <konradgrsk@gmail.com>
2021-10-08 16:15:01 +0200

    session store in mongo, helmet csp disabled

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import express from "express";
 import exphbs from "express-handlebars";
 import session from "express-session";
 import passport from "passport";
+import helmet from "helmet";
+
 import connectDB from "./config/db";
 import { initPassport } from "./config/passport";
 import log, { expressLogger } from "./logger";
@@ -12,6 +14,12 @@ import routes from "./routes";
 dotenv.config();
 const app = express();
 app.use(expressLogger);
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+app.use(express.json());
 app.use(express.static("public"));
 
 initPassport(passport);
@@ -28,25 +36,29 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", ".hbs");
 
 // Session
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET || "lady fingers",
-    resave: false,
-    saveUninitialized: true,
-  })
-);
+(async () => {
+  const store = await connectDB();
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET || "lady fingers",
+      resave: false,
+      saveUninitialized: true,
+      store,
+    })
+  );
 
-// Passport
-app.use(passport.initialize());
-app.use(passport.session());
+  // Passport
 
-// Routes
-app.use("/", routes);
+  app.use(passport.initialize());
+  app.use(passport.session());
+  // Routes
+  app.use("/", routes);
+})();
 
 const start = async () => {
   const PORT = process.env.PORT || 5000;
   try {
-    await connectDB();
+    // connectDB();
     app.listen(PORT, () => {
       log.info(`StoryBooks running on localhost:${PORT}`);
     });
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,17 +1,30 @@
 import mongoose from "mongoose";
+import MongoStore from "connect-mongo";
 import log from "../logger";
+
+const connectMongoose = async (mongoUrl: string) => {
+  try {
+    const conn = await mongoose.connect(mongoUrl);
+    log.info(`MongoDB connected: ${conn.connection.host}`);
+    const client = conn.connection.getClient();
+    return client;
+  } catch (error) {
+    log.error("Mongoose connect failed");
+    console.error(error);
+    process.exit(1);
+  }
+};
 
 const connectDB = async () => {
   const MONGO_URL = process.env.MONGO_URL;
   if (MONGO_URL) {
-    try {
-      const conn = await mongoose.connect(MONGO_URL);
-      log.info(`MongoDB connected: ${conn.connection.host}`);
-    } catch (error) {
-      log.error("Mongoose connect failed");
-      console.error(error);
-      process.exit(1);
-    }
+    await connectMongoose(MONGO_URL);
+
+    const store = MongoStore.create({
+      mongoUrl: MONGO_URL,
+    });
+
+    return store;
   } else {
     log.error("DB url not provided");
     process.exit(1);
